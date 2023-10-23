52571ad6f0c492ba1b618bbd6415ac5cc33224a8
Arya Tandale <93761073+aryatandale@users.noreply.github.com>
2023-10-23 22:44:30 +0530

    Add files via upload

---
+++ task3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient balance");
+        }
+    }
+
+    public double checkBalance() {
+        return balance;
+    }
+}
+
+
